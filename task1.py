163e7ace768562b95a826506526dc6a40724b69f
maksim <kuban.flowers@gmail.com>
2020-10-04 16:58:12 +0300

    Сделал домашнее задание после первого урока

---
+++ task1.py
@@ -0,0 +1,17 @@
+a = 2
+b = 2.1
+c = True
+d = "stroka"
+e = [1, False, [0, 1]]
+print("Тип переменной a: ", type(a))
+print("Тип переменной b: ", type(b))
+print("Тип переменной c: ", type(c))
+print("Тип переменной d: ", type(d))
+print("Тип переменной e: ", type(e))
+print("")
+
+first_name = input("Введите имя: ")
+last_name = input("Введите фамилию: ")
+age = int(input("Введите возраст: "))
+result = f"Ваше имя и фамилия {first_name} {last_name} , ваш возраст {age} года"
+print(result)
